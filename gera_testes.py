7ca5dbc0f130fec1fa0f6d0d6695034508858d1e
Daniel Sivaldi <danielferes@live.com>
2018-04-07 20:51:39 -0300

    adicionado gerador de arquivos de entrada

---
+++ gera_testes.py
@@ -0,0 +1,15 @@
+def cria_testes(n, alg):
+    for i in range(1,n):
+        arq = open('testes/test' + str(alg) + str(i) + '.txt', 'w')
+        arq.write(str(i*100) + '\n' + str(alg) + '\n')
+        arq.close()
+
+def main():
+    cria_testes(15, 1)
+    cria_testes(15, 2)
+    cria_testes(15, 3)
+
+if __name__ == '__main__':
+    main()
+
+# cria 15 arquivos de entrada pra cada algoritmo, variando o raio de 100 a 1500
